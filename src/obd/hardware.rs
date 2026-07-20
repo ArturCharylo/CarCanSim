31856299c4ad5e906ea5d04ee9ecfa7eae0dc043
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-07-20 11:20:10 +0200

    err handling: Added ObdError enum for handling errors correctly

--- src/obd/hardware.rs
+++ src/obd/hardware.rs
@@ -1,13 +1,24 @@
 use crate::obd::ObdInterface;
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ObdError{
+    #[error("Hardware disconnected")]
+    Disconnected,
+    #[error("Failed to parse metrics")]
+    ParseError,
+    #[error("Feature not implemented: {0}")]
+    NotImplemented(String),
+}
 
 pub struct HardwareAdapter;
 
 impl ObdInterface for HardwareAdapter {
-    fn read_engine_rpm(&self) -> Result<u32, String> {
-        Err("Hardware integration not implemented yet".to_string())
+    fn read_engine_rpm(&self) -> Result<u32, ObdError> {
+        Err(ObdError::NotImplemented("Hardware integration not implemented yet".to_string()))
     }
 
-    fn read_vehicle_speed(&self) -> Result<u8, String> {
-        Err("Hardware integration not implemented yet".to_string())
+    fn read_vehicle_speed(&self) -> Result<u8, ObdError> {
+        Err(ObdError::NotImplemented("Hardware integration not implemented yet".to_string()))
     }
 }
