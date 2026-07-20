daf192fee8f7c08bc19f444faf0e63ba0f982591
Artur <50996474+ArturCharylo@users.noreply.github.com>
2026-07-20 11:28:07 +0200

    fix: corrected types for error obdError

--- src/obd/mod.rs
+++ src/obd/mod.rs
@@ -1,7 +1,19 @@
 pub mod hardware;
 pub mod simulator;
 
+use thiserror::Error;
+
+#[derive(Error, Debug)]
+pub enum ObdError{
+    #[error("Hardware disconnected")]
+    Disconnected,
+    #[error("Failed to parse metrics")]
+    ParseError,
+    #[error("Feature not implemented: {0}")]
+    NotImplemented(String),
+}
+
 pub trait ObdInterface: Send + Sync {
-    fn read_engine_rpm(&self) -> Result<u32, String>;
-    fn read_vehicle_speed(&self) -> Result<u8, String>;
+    fn read_engine_rpm(&self) -> Result<u32, ObdError>;
+    fn read_vehicle_speed(&self) -> Result<u8, ObdError>;
 }
